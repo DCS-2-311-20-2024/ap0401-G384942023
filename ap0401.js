023bbfffaeb585592b67a3b83d101630c6aa148b
G384942023 <238494@st.takushoku-u.ac.jp>
2024-10-23 16:01:30 +0900

    第4回前半
    学生番号、氏名の変更
    枠の追加
    ボールが動かす
    ボールが枠で跳ね返るよう変更
    パドルの追加
    パドルにマウスを追従させる
    パドルにボールを打ち返させる
    ボールを受け損なった時の処理を追加
    ボールの初期位置がパドルを追従するよう変更

--- ap0401.js
+++ ap0401.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第4回 課題 (ap0401)
-// G384002023 拓殖太郎
+// G384942023 村上瑛
 //
 "use strict"; // 厳格モード
 
@@ -55,6 +55,14 @@ function init() {
   let vz = -Math.cos(pi / 4);
 
   function moveBall(delta) {
+    if (ballLive) {
+      vBall.set(vx, 0, vz)
+      ball.position.addScaledVector(vBall, delta * speed);
+    }
+    else {
+      ball.position.x = paddle.position.x;
+      ball.position.z = paddle.position.z - ballR - paddleR;
+    }
   }
 
   // ボールの死活
@@ -63,10 +71,14 @@ function init() {
 
   // ボールを停止する
   function stopBall() {
+    speed = 0;
+    ballLive = false;
   }
 
   // ボールを動かす
   function startBall() {
+    ballLive = true;
+    speed = 10;
   }
 
   // マウスクリックでスタートする
@@ -96,8 +108,13 @@ function init() {
       new THREE.BoxGeometry(vFrameW, vFrameH, vFrameD),
       new MeshPhongMaterial({ color: 0xB3B3B3 })
     );
+    lFrame.position.x = -(hFrameW/2-vFrameW/2);
+    scene.add(lFrame);
 
     //   右の枠
+    const rFrame = lFrame.clone();
+    rFrame.position.x = hFrameW/2-vFrameW/2;
+    scene.add(rFrame);
 
   }
 
@@ -106,12 +123,29 @@ function init() {
   const vLimit = vFrameD / 2;
   function frameCheck() {
     // 右
+    if (ball.position.x + ballR > hLimit){
+      ball.position.x = hLimit - ballR;
+      vx = -Math.abs(vx);
+    }
 
     // 左
+    if (ball.position.x - ballR < -hLimit){
+      ball.position.x = -hLimit + ballR;
+      vx = Math.abs(vx);
+    }
 
     // 上
+    if (ball.position.z - ballR < -vLimit){
+      ball.position.z = -vLimit + ballR;
+      vz = Math.abs(vz);
+    }
 
     // 下
+    if (ball.position.z + ballR > vLimit){
+      stopBall();
+      //ball.position.z = vLimit - ballR;
+      //vz = -Math.abs(vz);
+    }
 
   }
 
@@ -126,6 +160,8 @@ function init() {
       new THREE.CylinderGeometry(paddleR, paddleR, paddleL, nSeg),
       new THREE.MeshPhongMaterial({ color: 0x333333, shininess: 100, specular: 0x404040 })
     );
+    center.rotation.z=Math.PI/2;
+    paddle.add(center);
 
     // パドル端
     const sideGeometry
@@ -133,10 +169,19 @@ function init() {
     const sideMaterial
       = new THREE.MeshPhongMaterial({ color: 0x666666, shininess: 100, specular: 0xa0a0a0 })
     // パドル端(右)
+    const right = new THREE.Mesh(sideGeometry,sideMaterial);
+    right.position.x = paddleL/2;
+    paddle.add(right);
 
     // パドル端(左)
+    const left = new THREE.Mesh(sideGeometry,sideMaterial);
+    left.rotation.z = Math.PI;
+    left.position.x = -paddleL/2;
+    paddle.add(left);
 
     // パドルの配置
+    paddle.position.z = 0.4 * vFrameD;
+    scene.add(paddle);
 
   }
   
@@ -148,14 +193,38 @@ function init() {
     const raycaster = new THREE.Raycaster();
     const intersects = new THREE.Vector3();
     function paddleMove(event) {
-  
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      raycaster.setFromCamera(mouse, camera);
+      raycaster.ray.intersectPlane(plane, intersects);
+      const offset = hFrameW / 2 - vFrameW - paddleL / 2 - paddleR;
+      if(intersects.x < -offset){
+        intersects.x = -offset;
+      }
+      else if (intersects.x > offset) {
+        intersects.x = offset;
+      }
+      paddle.position.x = intersects.x;
     }
     window.addEventListener("mousemove", paddleMove, false);
   }
 
   // パドルの衝突検出
   function paddleCheck() {
-  
+    if (Math.abs(ball.position.z - paddle.position.z) < paddleR +ballR &&
+        Math.abs(ball.position.x - paddle.position.x) < paddleL/2 + ballR ) {
+          //中央部分と衝突
+          if ( ball.position.z < paddle.position.z ) {
+            vz = -Math.abs(vz);
+          }
+          //右側部分と衝突
+          if ( ball.position.x > paddle.position.x + paddleL/2 ) {
+            vx = Math.abs(vx);
+          }
+          //左側部分と衝突
+          else if ( ball.position.x < paddle.position.x - paddleL/2 ) {
+            vx = -Math.abs(vx);
+          }
+        }
   }
 
   // ブロック ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
